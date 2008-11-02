dcd847e3c8e73f80bcd2e03b6b31f6b7de6abbac
Thijs Van der Schaeghe <thijs@catlab.be>
2008-11-02 18:20:45 +0000

    even more fix
    git-svn-id: https://catlab.slsapp.com/source/1brood/trunk@54 d97fe4b3-058e-4413-8caa-47a303eb7676

--- php/config.php
+++ php/config.php
@@ -63,7 +63,6 @@ if (isset ($_GET['language']))
 		&& is_dir (LANGUAGE_DIR . $_GET['language'])
 	)
 	{
-		setCookie ('language', $_GET['language'], time () + 60*60*24*365);
 		setCookie ('language', $_GET['language'], time () + 60*60*24*365, '/');
 		$_COOKIE['language'] = $_GET['language'];
 	}
@@ -77,7 +76,6 @@ if (isset ($_GET['layout']))
 		&& is_dir (TEMPLATES_DIR . $_GET['layout'])
 	)
 	{
-		setCookie ('layout', $_GET['layout'], time () + 60*60*24*365);
 		setCookie ('layout', $_GET['layout'], time () + 60*60*24*365, '/');
 		$_COOKIE['layout'] = $_GET['layout'];
 	}

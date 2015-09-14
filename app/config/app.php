8cb1063dc2c5bdba7db15b57cef70041cbbf8866
Thijs Van der Schaeghe <thijs@catlab.be>
2015-09-14 23:52:40 +0200

    Adding heroku support.

--- app/config/app.php
+++ app/config/app.php
@@ -2,7 +2,7 @@
 
 if (
 	isset ($_SERVER['HTTPS']) ||
-	(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == "https")
+	(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] != "http")
 ) {
 	$absluteUrl = 'https';
 } else {

97bf7965d9f9b9c9306396c7270dec8f179bc7ed
daedeloth <thijs@catlab.be>
2017-05-19 12:34:50 +0200

    Order companies by name.

--- app/config/mailer.php
+++ app/config/mailer.php
@@ -9,8 +9,8 @@ return array (
 		    'server' => getenv("SMTP_SERVER"),
 			'username' => getenv("SMTP_USERNAME"),
 			'password' => getenv("SMTP_PASSWORD"),
-            'security' => getenv("SMTP_SECURITY") ? getenv("SMTP_SECURITY") : 'ssl'),
-            'port' => getenv("SMTP_PORT") ? getenv("SMTP_PORT") : 465),
+            'security' => (getenv("SMTP_SECURITY") ? getenv("SMTP_SECURITY") : 'ssl'),
+            'port' => (getenv("SMTP_PORT") ? getenv("SMTP_PORT") : 465)
 		)
 	)
 

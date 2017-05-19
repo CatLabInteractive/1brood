217d422b2165b0cd76777be10af42a35befc7d0b
daedeloth <thijs@catlab.be>
2017-05-19 12:32:25 +0200

    Order companies by name.

--- app/config/mailer.php
+++ app/config/mailer.php
@@ -7,9 +7,12 @@ return array (
 	'from' => 'info@1brood.be',
 
 	'services' => array (
-		'mandrill' => array (
-			'username' => $username,
-			'key' => $key
+		'smtp' => array (
+		    'server' => getenv("SMTP_SERVER"),
+			'username' => getenv("SMTP_USERNAME"),
+			'password' => getenv("SMTP_PASSWORD"),
+            'security' => getenv("SMTP_SECURITY") ? getenv("SMTP_SECURITY") : 'ssl'),
+            'port' => getenv("SMTP_PORT") ? getenv("SMTP_PORT") : 465),
 		)
 	)
 
